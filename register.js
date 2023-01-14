b976223189ff82f98de70fcb2a0b9ef4dad9a7e4
vineethkumar12 <adepuvineethkumarvinni@gmail.com>
2023-01-14 19:32:08 +0530

    updated

---
+++ register.js
@@ -0,0 +1,41 @@
+const handleregister=(req,res,db)=>{ 
+    const {email,password,name}=req.body
+      if(!email || !password|| !name)
+   {
+    return res.status(400).json("unable to get registers")
+   }
+  
+  
+          // Store hash in your password DB. 
+         
+       
+    
+     db.transaction(trx=>{
+      trx.insert({
+    
+        password:password,
+        email:email,
+  
+  
+      })
+      .into('login')
+      .returning('email')
+      .then( loginEmail=>{
+       return  trx('users').returning('*')
+       .insert({
+          name:name,
+          email:loginEmail[0].email,
+         joined:new Date()})
+        .then(user=>{
+          res.json(user[0])
+         })
+  
+      }).then(trx.commit)
+      .catch(trx.rollback)
+     })
+      .catch(err => res.status(400).json(err))
+    }
+
+     module.exports={
+     handleregister:handleregister
+     };
--- server.js
+++ server.js
@@ -2,6 +2,44 @@ const bodyParser = require('body-parser')
 const express =require('express') 
 const cors =require('cors') 
 const bcrypt=require('bcrypt')
+const {knex}=require('knex')
+const register=require('./register')
+//const {Client} =require('pg')
+const db=knex({
+  client: 'pg',
+    connection: {
+      host : '127.0.0.1',
+      user : 'postgres',
+      password : 'vinni@123#',
+      database : 'facedatabase'
+    }
+   
+
+})
+
+/*const client =new Client({
+  
+ 
+      host : 'localhost',
+      port:5432,
+      user : 'postgres',
+      password : 'vinni@123#',
+      database : 'facedatabase'
+
+  
+
+
+})
+client.connect();
+client.query('select * from public.users', (err,result)=>{
+   if(!err)
+   {
+    console.log(result.rows);
+   }
+   client.end();
+
+})*/
+//db.select('*').from('public.users').then(data=>console.log(data))
 const app = express()   
  app.use(express.json())
  app.use(cors())
@@ -40,104 +78,69 @@ const database= {
   }
   ]
 } 
-app.get("/signin",(req,res)=>{ 
+app.post("/signin",(req,res)=>{ 
     const {email,password}=req.body 
-   
- bcrypt.compare("vini", '$2b$10$gQxiwOOh3SuNeRcpD9iUd..XVBNpbOha6TPtE0/5IHhbhW9H21YFW', function(err, result) {
-  console.log("same"+result)
-});
-bcrypt.compare('vinnih','$2b$10$gQxiwOOh3SuNeRcpD9iUd..XVBNpbOha6TPtE0/5IHhbhW9H21YFW', function(err, result) {
-  // result == false 
-  console.log(" not same"+result) 
- 
-});  
-    //var a=false
-//database.users.filter((user,i)=>{ 
-      
-  //if(password === user.password && email === user.email)
-  //{     a = true
-    return  res.json(database.users)
-  //} 
-   
- 
-   
-//} )   
-//if (!a) 
-//   return res.json(database.users)
-   
+    db.select('email','password').from('login').where('email','=',email)
+    .then(data=>{
+     
+      if(data[0].password===password)
+        {return db.select('*').from('users').where('email','=',email)
+        .then(user => res.json(user[0]))
+        .catch(err=>res.status(400).json(err))
+        
+          
+        }
+        else {
+          res.status(400).json('invalid user')
+        }
+    }).catch(err=>res.status(400).json(err))
   
 
+   // return  res.json(database.users)
+ 
 
 }) 
     
- app.post("/register",(req,res)=>{ 
-    const {email,password,name}=req.body 
-    
-   
-     database.users.push(
-        {
-            id: "125",
-            name:name,
-            email:email,
-            password:password,
-            entries:"0",
-            joined:new Date()
-         
-           }  
-           
+app.post("/register",(req,res)=>{register.handleregister(req,res,db)}) 
+  
+  
 
-     ) 
-     bcrypt.genSalt( function(err, salt) {
-      bcrypt.hash(password, salt, function(err, hash) {
-          // Store hash in your password DB. 
-          console.log(hash)
-      });
-  });
-        res.json(database.users[database.users.length-1])
+
+
+
+       
+    
         
-        }) 
+        
+        
+
         app.get("/profile/:id",(req,res)=>{ 
         const {id}= req.params 
         var  newdata=false
-           database.users.filter(item=>{ 
-              if(item.id=== id) 
-             {    newdata=true; 
-               
-                return  res.json(item) ;}
-             
-             } )  
-             if( !newdata) 
-             { res.json(" id is not valid")} 
-            })   
+           db.select('*').from('users').where({id})
+           .then(user => res.json(user[0]))
+           .catch(err=>res.status(400).json(err))
+           
+           /*if( !newdata) 
+             { res.json(" id is not valid")} */
+            }
+            )  
+
+           
             app.put("/image",(req,res)=>{ 
                 const {id}= req.body  
-                var  newdata=false
-             database.users.filter(item=>{ 
-              if(item.id=== id) 
-              {    newdata=true; 
-                        var a=item.entries++
-                        return  res.json(a) ;}
-                     
-              } )  
-                     if( !newdata) 
-                     { res.json(" id is not valid")} 
-                    })   
-
-
+                
+                  db('users').where('id','=',id).increment('entries',1).returning('entries')
+            .then(entries => {
+              
+              res.json(entries[0])
+            })
+            .catch(err=>res.status(400).json("unable to get entries"))})
+              
     
      app.get("/",(req,res)=>{
      res.send(database.users) }) 
 
-    // bcrypt.hash(myPlaintextPassword, saltRounds, function(err, hash) {
-      // Store hash in your password DB.
-  //}); 
-  // Load hash from your password DB.
-//bcrypt.compare(myPlaintextPassword, hash, function(err, result) {
-  // result == true
-//});
-//bcrypt.compare(someOtherPlaintextPassword, hash, function(err, result) {
-  // result == false
-//});
 
     
 
