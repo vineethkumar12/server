50945457948aa196401595d0742fc2997353f92c
vineethkumar12 <adepuvineethkumarvinni@gmail.com>
2024-04-07 03:16:10 +0530

    updated2

--- server.js
+++ server.js
@@ -9,7 +9,11 @@ require("dotenv").config();
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://vineethkumar12.github.io",
+  })
+);
 
 const db = knex({
   client: "pg",
