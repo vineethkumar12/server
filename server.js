028316ee68fcb6968f918bf1e18d1efe5b3c1092
vineethkumar12 <adepuvineethkumarvinni@gmail.com>
2024-04-07 02:36:15 +0530

    updated

--- server.js
+++ server.js
@@ -13,10 +13,11 @@ require("dotenv").config();
 const db = knex({
   client: "pg",
   connection: {
-    host: process.env.DB_HOST || "127.0.0.1",
+    host: process.env.DB_URL || "127.0.0.1",
     user: process.env.DB_USER || "postgres",
     password: process.env.DB_PASSWORD || "vinni@123#",
     database: process.env.DB_NAME || "facedatabase",
+    port: process.env.PORT,
   },
 });
 
